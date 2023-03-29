ccb6c1482d9a5a891de94e47c3e9eac16336652e
HannahVitalos <vitalos@marshall.edu>
2023-03-29 16:15:03 -0400

    repoSetup

---
+++ bloodbank/bloodbankApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path(r'', views.home, name='home'),
+    path(r'login/', views.login, name='login'),
+    path(r'donorsignup/', views.donorSign, name='donorSignUp'),
+    path(r'volunteer/', views.volunteer, name='volunteer'),
+]
---
+++ bloodbank/bloodbankApp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+#views list: homepage (display bloodbank info, login button, donor signup button, volunteer signup button),
+#login page, donor sign up page, volunteer sign up page, sign up success page
+#signed in views: donors list page, blood list page, patients list page, volunteers list page, recipients list page
